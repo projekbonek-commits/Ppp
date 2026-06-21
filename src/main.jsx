ade64e506cd342ee3c4ba1611b9dc1864d86fb48
projekbonek-commits <projekbonek@gmail.com>
2026-06-21 21:56:04 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // Mengimpor file App.jsx yang kita buat tadi
+import App from './App.tsx'; // Mengimpor file App.jsx yang kita buat tadi
 import './index.css';        // Jika Anda menggunakan Tailwind
 
 ReactDOM.createRoot(document.getElementById('root')).render(
